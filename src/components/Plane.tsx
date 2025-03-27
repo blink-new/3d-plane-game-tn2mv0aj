8c4c65aa5e830241f6cd992894e108fb2f0d56db
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-03-27 23:10:43 +0000

    Fix Dark Scene and Lighting

--- src/components/Plane.tsx
+++ src/components/Plane.tsx
@@ -6,45 +6,45 @@ import * as THREE from 'three'
 export const Plane = forwardRef<THREE.Group>((props, ref) => {
   return (
     <group ref={ref} {...props}>
-      {/* Main body - brighter colors */}
-      <mesh castShadow position={[0, 0, 0]}>
+      {/* Main body */}
+      <mesh castShadow receiveShadow position={[0, 0, 0]}>
         <cylinderGeometry args={[0.2, 0.5, 2, 8]} />
         <meshStandardMaterial 
-          color="#64B5F6" 
-          metalness={0.4} 
-          roughness={0.2} 
-          emissive="#64B5F6"
+          color="#4287f5"
+          metalness={0.6}
+          roughness={0.2}
+          emissive="#4287f5"
           emissiveIntensity={0.2}
         />
       </mesh>
 
-      {/* Wings - brighter colors */}
-      <mesh castShadow position={[0, 0, 0]}>
+      {/* Wings */}
+      <mesh castShadow receiveShadow position={[0, 0, 0]}>
         <boxGeometry args={[4, 0.1, 1]} />
         <meshStandardMaterial 
-          color="#2196F3" 
-          metalness={0.4} 
+          color="#2196F3"
+          metalness={0.6}
           roughness={0.2}
           emissive="#2196F3"
           emissiveIntensity={0.2}
         />
       </mesh>
 
-      {/* Tail - brighter colors */}
-      <mesh castShadow position={[0, 0.4, -1]}>
+      {/* Tail */}
+      <mesh castShadow receiveShadow position={[0, 0.4, -1]}>
         <boxGeometry args={[1, 0.8, 0.1]} />
         <meshStandardMaterial 
-          color="#2196F3" 
-          metalness={0.4} 
+          color="#2196F3"
+          metalness={0.6}
           roughness={0.2}
           emissive="#2196F3"
           emissiveIntensity={0.2}
         />
       </mesh>
 
-      {/* Brighter engine trails */}
-      <pointLight position={[-1, 0, -1]} color="#00ff00" intensity={3} distance={5} />
-      <pointLight position={[1, 0, -1]} color="#00ff00" intensity={3} distance={5} />
+      {/* Engine glow effects */}
+      <pointLight position={[-1, 0, -1]} color="#00ff00" intensity={2} distance={3} />
+      <pointLight position={[1, 0, -1]} color="#00ff00" intensity={2} distance={3} />
     </group>
   )
 })
